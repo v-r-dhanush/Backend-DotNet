8bcf89575447675981ba2289f20614bffed50f8c
dhanush <dhanushravi647@gmail.com>
2025-01-08 18:27:53 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+using MakeMyTrip.Models;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace MakemytripApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> UpdateReservation(int id)
+        {
+            Reservation reservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5004/Api/Reservation/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(reservation);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteReservation(int ReservationId)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.DeleteAsync("http://localhost:5004/Api/Reservation/" + ReservationId))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(reservation.Id.ToString()), "Id");
+                content.Add(new StringContent(reservation.Name), "Name");
+                content.Add(new StringContent(reservation.StartLocation), "StartLocation");
+                content.Add(new StringContent(reservation.EndLocation), "EndLocation");
+
+                using (var response = await httpClient.PutAsync("http://localhost:5004/Api/Reservation/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Result = "Success";
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+        public ViewResult AddReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> AddReservation(Reservation reservation)
+        {
+            Reservation receivedReservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(reservation), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("http://localhost:5004/Api/Reservation/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    receivedReservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                }
+            }
+            return View(receivedReservation);
+        }
+
+        public ViewResult GetReservation() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> GetReservation(int id)
+        {
+            Reservation reservation = new Reservation();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5004/Api/Reservation/" + id))
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        reservation = JsonConvert.DeserializeObject<Reservation>(apiResponse);
+                    }
+                    else
+                        ViewBag.StatusCode = response.StatusCode;
+                }
+
+            }
+            return View(reservation);
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Reservation> reservationList = new List<Reservation>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5004/Api/Reservation/"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<Reservation>>(apiResponse);
+                }
+            }
+            return View(reservationList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
